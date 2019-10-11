589fac7c2f43f4fd2da33c58210ba2401de916ba
Kunaala <kunaalmalodhakar@gmail.com>
2019-10-11 11:25:57 +0530

    resolved bugs

--- rosimgs/imgstreamer/installopencv.sh
+++ rosimgs/imgstreamer/installopencv.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 git clone https://github.com/opencv/opencv.git
+pwd 
 cd opencv
+pwd
 mkdir build
 cd build
 cmake -D CMAKE_BUILD_TYPE=RELEASE \
@@ -8,8 +10,8 @@ cmake -D CMAKE_BUILD_TYPE=RELEASE \
             -D INSTALL_C_EXAMPLES=ON \
             -D WITH_V4L=ON \
             -D OPENCV_PYTHON3_INSTALL_PATH=$cwd/OpenCV-$cvVersion-py3/lib/python3.5/site-packages \
-#        -D WITH_QT=ON \
+        -D WITH_QT=ON \
         -D WITH_OPENGL=ON \
-        -D BUILD_EXAMPLES=ON ..
+ /host/opencv
 make  -j'nproc'
 make install 
